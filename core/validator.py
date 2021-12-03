2b9f2319c0aeba1a19312b1fa901b70beb976fea
Ted0527 <frosthowling@gmail.com>
2021-12-03 20:07:24 +0900

    [Add]added validator.py

--- core/validator.py
+++ core/validator.py
@@ -4,14 +4,10 @@ from django.core.exceptions import ValidationError
 
 from users.models           import User
 
-REGEX_EMAIL = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-# 대,소문자, 숫자, -, _, ., 허용, (@, .) 필수 입력
+REGEX_EMAIL    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
 REGEX_PASSWORD = '/^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/'
-# 대,소문자, 숫자 그리고 괄호를 제외한 1개 이상의 특수문자 필요, 8자리 이상, 15자리 이하 비밀번호
 REGEX_USERNAME = '/^(?=.*[a-zA-Z])[-a-zA-Z0-9_.]{4,10}$/'
-# 대,소문자, 숫자, 특문포함
-REGEX_CONTACT = '/^\d{2,3}-\d{3,4}-\d{4}$/'
-# 앞자리 2~3자리 가능(폰번호 대신 집전화 쓸 수도 있어서)
+REGEX_CONTACT  = '/^\d{2,3}-\d{3,4}-\d{4}$/'
 
 def validates_email(email):
     if not re.match(REGEX_EMAIL, email) or email is None:
