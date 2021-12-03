1d27f8f61814422af3fab897135f4073286c9858
JongHo Seong <jonghoseong@JongHoui-MacBookAir.local>
2021-12-03 16:07:45 +0900

    [modify]product_detail_view

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import ProductDetailView
 
 urlpatterns = [
-  path("/product",ProductDetailView.as_view(),)
+  path("/<int:product_id>",ProductDetailView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -4,24 +4,27 @@ from django.views import View
 from .models import Product
 
 class ProductDetailView(View):
-  def get(self,request):
-    try:
-      print(request.GET.get("id"))
-      product=Product.objects.prefetch_related("image_set").get(id=request.GET.get("id"))
-      
-      result = {
-        "product_name"         : product.name,
-        "product_price"        : product.price,
-        "product_introduction" : product.introduction,
-        "product_description"  : product.description,
-        "product_unit"         : product.unit,
-        "product_shipping"     : product.shipping,
-        "product_package"      : product.package,
-        "product_origin"       : product.origin,
-        "product_weight"       : product.weight,
-        "image_url"            : [image.url for image in product.image_set.all()]
-      } 
-      return JsonResponse({"result":result},status=200)
+    def get(self, request, product_id):
+        try:
+            product = Product.objects.get(id=product_id)
+            
+            result = {
+              "id"           : product.id,
+              "name"         : product.name,
+              "price"        : product.price,
+              "introduction" : product.introduction,
+              "description"  : product.description,
+              "unit"         : product.unit,
+              "shipping"     : product.shipping,
+              "package"      : product.package,
+              "origin"       : product.origin,
+              "weight"       : product.weight,
+              "images"       : [image.url for image in product.image_set.all()]
+            }
 
-    except Product.DoesNotExist : return JsonResponse({"message" : "INVALID_PRODUCT"},status = 401)
-    except KeyError             : return JsonResponse({"message" : "KEY_ERROR"},status = 400)
+            return JsonResponse({"result": result}, status=200)
+
+        except Product.DoesNotExist:
+            return JsonResponse({"message" : "INVALID_PRODUCT"}, status = 401)
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
