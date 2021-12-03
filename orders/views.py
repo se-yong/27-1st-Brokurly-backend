9a264046fdf48f19ef2e1d9086c04e71d0c93f63
JongHo Seong <jonghoseong@JongHoui-MacBookAir.local>
2021-12-03 18:34:49 +0900

    [modify]orders-modified

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http  import JsonResponse
+from django.views import View
 
+from .models      import Order, OrderStatus, OrderItem, OrderItemsStatus
+from users.models import User
+from cart.models  import Cart
+
+class OrderView(View):
+    # @데코레이터(token) 들어갈 자리
+    def post(self, request):
+        
+        return JsonResponse({},status=200)
 # Create your views here.
