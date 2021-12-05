9a400dd64ddbee0b3bcb71d34c44409d0209a4b4
Ted0527 <frosthowling@gmail.com>
2021-12-05 19:30:26 +0900

    [Add]Created signin decorator

--- core/decorator.py
+++ core/decorator.py
@@ -1,15 +1,33 @@
-import jwt, bcrypt
+import jwt
 
-from my_settings import SECRET_KEY, ALGORITHM
+from django.http    import JsonResponse
 
+# from json.decoder   import JSONDecodeError
 
+from jwt.exceptions import DecodeError
+
+from users.models import User
+from my_settings  import SECRET_KEY, ALGORITHM
 
 def signin(func):
     def wrapper(self, request, *args, **kwargs):
         try:
-            decode_password = jwt.encode('utf-8')(hashed_password, SECRET_KEY, algorithm=ALGORITHM)
+            access_token = request.headers.value('token') # FE에서 보내준 token
+            decode_token = jwt.decode('utf-8')(access_token, SECRET_KEY, algorithm=ALGORITHM)
+            request.user = User.objects.get(id=decode_token['id'])
 
+            if 'token' not in request.headers:
+                return JsonResponse({'message':'Token not Exist'}, status=404)
 
+            return func(self, request, *args, **kwargs)
 
-        except 
+        except User.DoesNotExist:
+            return JsonResponse({'message':'Invalid User'}, status=400)
+
+        except DecodeError:
+            return JsonResponse({'message':'Invalid Token'}, status=400)
+
+        # except JSONDecodeError:
+        #     return JsonResponse({'message':'JSONDecodeError'}, status=404)
         
+    return wrapper
