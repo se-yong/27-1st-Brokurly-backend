b8923550bdd59a6fba723b2ae796c70ed56256d5
JongHo Seong <jonghoseong@JongHoui-MacBookAir.local>
2021-12-06 16:50:52 +0900

    [modify]jongho-order-view

--- orders/views.py
+++ orders/views.py
@@ -1,28 +1,34 @@
 import json
+from datetime import datetime
 
 from django.http            import JsonResponse
 from django.views           import View
 from django.db              import transaction, DataError
+from django.core.exceptions import MultipleObjectsReturned
 
 from .models     import Order, OrderStatus, OrderItem, OrderItemsStatus
 from cart.models import Cart
+from users.models import User
+from products.models import Product
 
 class OrderView(View):
     # @데코레이터(token) 들어갈 자리
     def post(self, request):
         try:
+            print(datetime.now())
             data                 = json.loads(request.body)
             order_status         = OrderStatus.objects.get(id=1)
             order_items_status   = OrderItemsStatus.objects.get(id=1)
-
+            
             with transaction.atomic():
-                order = Order.objects.create(order_status=order_status, users=request.user)
+                order = Order.objects.create(order_status=order_status, users=User.objects.get(id=1))
                 bulk_order_item_list = []
             
                 for item in data:
+                    product = Product.objects.get(id=item["product_id"])
                     bulk_order_item_list.append(
                         OrderItem(
-                            product            = item["product_id"],
+                            product            = product,
                             quantity           = item["quantity"],
                             order              = order,
                             order_items_status = order_items_status,
@@ -35,13 +41,15 @@ class OrderView(View):
         except KeyError:
             return JsonResponse({"message":"KEY_ERROR"},status=400)
         except OrderStatus.DoesNotExist:
-            return JsonResponse({"message":"INVALID_ORDER_STATUS"},status=401)
+            return JsonResponse({"message":"INVALID_ORDER_STATUS"},status=404)
         except OrderItemsStatus.DoesNotExist:
-            return JsonResponse({"message":"INVALID_ORDER_ITEMS_STATUS"},status=401)
+            return JsonResponse({"message":"INVALID_ORDER_ITEMS_STATUS"},status=404)
         except Cart.DoesNotExist:
-            return JsonResponse({"message":"INVALID_CART"},status=401)
+            return JsonResponse({"message":"INVALID_CART"},status=404)
+        except MultipleObjectsReturned:
+            return JsonResponse({"message":"INVALID_ORDER"},status=400)
         except DataError:
-            return JsonResponse({"message":"DATA_ERROR"},status = 401)
+            return JsonResponse({"message":"DATA_ERROR"},status = 400)
         except transaction.TransactionManagementError:
             return JsonResponse({"message":"TRANSACTION_ERROR"},status=400)
     
@@ -55,34 +63,29 @@ class OrderView(View):
             
             result=[]
             if not orders.exists():
-                return JsonResponse({"message":result},status=201)
+                return JsonResponse({"message":result},status=204)
                 
             for order in orders:
                 resultItem={
                     "order_id"     : order.id,
                     "order_number" : order.order_number,
                     "order_status" : order.order_status.status,
-                    } [{
-                        
-                    }]
-
-                resultItem_list=[]
-                for orderItem in order.orderitem_set.all():
-                    resultItem_list.append({
-                        "id"       : orderItem.product.id,
-                        "name"     : orderItem.product.name,
-                        "image"    : orderItem.product.image_set.all()[0].url,
-                        "price"    : orderItem.product.price,
-                        "quentity" : orderItem.quantity,
-                        "status"   : orderItem.order_items_status.status,
-                    })
+                    } 
+                resultItem_list=[{
+                    "id"       : orderItem.product.id,
+                    "name"     : orderItem.product.name,
+                    "image"    : orderItem.product.image_set.all()[0].url,
+                    "price"    : orderItem.product.price,
+                    "quentity" : orderItem.quantity,
+                    "status"   : orderItem.order_items_status.status,
+                    }for orderItem in order]
                 resultItem["products"] = resultItem_list
                 result.append(resultItem)
 
-            return JsonResponse({"message":result},status=201)
+            return JsonResponse({"message":result},status=200)
             
         except Exception:
-            return JsonResponse({"message":"ERROR"},status=401)
+            return JsonResponse({"message":"ERROR"},status=400)
 
     # @데코레이터 들어갈자리
     def patch(self, request):
@@ -103,8 +106,10 @@ class OrderView(View):
         except KeyError: 
             return JsonResponse({"message":"KEY_ERROR"},status=400)
         except OrderStatus.DoesNotExist:
-            return JsonResponse({"message":"INVALID_ORDER_STATUS"},status=401)
+            return JsonResponse({"message":"INVALID_ORDER_STATUS"},status=404)
         except OrderItemsStatus.DoesNotExist:
-            return JsonResponse({"message":"INVALID_ORDER_ITEMS_STATUS"},status=401)
+            return JsonResponse({"message":"INVALID_ORDER_ITEMS_STATUS"},status=404)
+        except MultipleObjectsReturned:
+            return JsonResponse({"message":"INVALID_ORDER"},status=400)
         except transaction.TransactionManagementError:
-            return JsonResponse({"message":"TRANSACTION_ERROR"},status=401)
+            return JsonResponse({"message":"TRANSACTION_ERROR"},status=400)
