ff143a347635ed2d9cf20317e9184057ecd381c5
Ted0527 <frosthowling@gmail.com>
2021-12-07 11:45:21 +0900

    [fixing]

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.urls    import path
-from products.views import ProductView
+from products.models import Category
+from products.views import ProductView, CategoryView
 
 urlpatterns = [
-    path('', ProductView.as_view())
+    path('', ProductView.as_view()),
+    path('/category', CategoryView.as_view()),
+
 ]
--- products/views.py
+++ products/views.py
@@ -6,45 +6,102 @@ from products.models import Product, Category, Menu
 class ProductView(View):
     def get(self, request):
         try:
-            category_id = request.GET.get('category', 'all')
+            menu_name = request.GET.get('menu', None)
+            sort      = request.GET.get('sort', 0)
+            sort_dict = {
+                0 : 'created_at',
+                1 : 'price',
+                2 : '-price',
+            }
+
+            if menu_name == '채소전체':
+                menu_name = '채소'
+                menus = Menu.objects.get(name=menu_name)
+                categories = Category.objects.filter(menu=menus).all()
+                products = Product.objects.filter(category__in=categories).all().order_by(sort_dict[sort])
+                results = []
+
+            elif menu_name == '샐러드전체':
+                menu_name = '샐러드'
+                menus = Menu.objects.get(name=menu_name)
+                categories = Category.objects.filter(menu=menus).all()
+                products = Product.objects.filter(category__in=categories).all().order_by(sort_dict[sort])
+                results = []
+
+            elif menu_name == '과일전체':
+                menu_name = '과일'
+                menus = Menu.objects.get(name=menu_name)
+                categories = Category.objects.filter(menu=menus).all()
+                products = Product.objects.filter(category__in=categories).all().order_by(sort_dict[sort])
+                results = []
+
+            elif menu_name == '간편식전체':
+                menu_name = '간편식'
+                menus = Menu.objects.get(name=menu_name)
+                categories = Category.objects.filter(menu=menus).all()
+                products = Product.objects.filter(category__in=categories).all().order_by(sort_dict[sort])
+                results = []
+
+            if menu_name:
+                menus = Menu.objects.get(name=menu_name)
+                categories = Category.objects.filter(menu=menus).all()
+                products = Product.objects.filter(category__in=categories).all().order_by(sort_dict[sort])
+                # products = Product.objects.filter(category__menu_id=menu_name).all()
+                results = []
+
+            for product_data in products:
+                results.append(
+                    {
+                        'name'         : product_data.name,
+                        'introduction' : product_data.introduction,
+                        'price'        : product_data.price,
+                        'image'        : [image.url for image in product_data.image_set.all()]
+                    }
+                )
+            print(results)
+            return JsonResponse({'result':results}, status=201)
+
+        except AttributeError:
+            return JsonResponse({'message' : 'AttributeError'}, status=400)
+        
+        except TypeError:
+            return JsonResponse({'message' : 'TypeError'}, status=400)
+
+        except Category.DoesNotExist:
+            return JsonResponse({'message' : 'DoesNotExits'}, status=500)
+
+class CategoryView(View):
+    def get(self, request):
+        try:
+            category_name = request.GET.get('category', None)
             sort        = request.GET.get('sort', 0)
             sort_dict   = {
                 0 : 'created_at',
                 1 : 'price',
                 2 : '-price',
-                3 : 'name'
             }
-
-            if category_id == 'all':
-                products = Product.objects.all()
+                
+            if category_name == 'all':
+                categories = Category.objects.get(name='쌈채소')
+                products = Product.objects.filter(category=categories).all().order_by(sort_dict[sort])
+                # products = Product.objects.filter(category_id=category_id, category__menu_id=menu_id)
                 results = []
-                for data_all in products:
-                    results.append(
-                            {
-                                'name'         : data_all.name,
-                                'introduction' : data_all.introduction,
-                                'price'        : data_all.price,
-                                'image'        : [image.url for image in data_all.image_set.all()]
-                            }
-                        )
-                print(results)
-                return JsonResponse({'result':results}, status=201)
 
+            if category_name:
+                categories = Category.objects.get(name=category_name)
+                products = Product.objects.filter(category=categories).all().order_by(sort_dict[sort])
+                # products = Product.objects.filter(category_id=category_id, category__menu_id=menu_id).all().order_by(sort_dict[sort])
+                results = []
 
-
-            category = Category.objects.get(name=category_id)
-            products = Product.objects.filter(category=category).all().order_by(sort_dict[sort])
-            results  = []
             for product_data in products:
                 results.append(
-                            {
-                                'name'         : product_data.name,
-                                'introduction' : product_data.introduction,
-                                'price'        : product_data.price,
-                                'image'        : [image.url for image in product_data.image_set.all()]
-                            }
-                        )
-
+                    {
+                        'name'         : product_data.name,
+                        'introduction' : product_data.introduction,
+                        'price'        : product_data.price,
+                        'image'        : [image.url for image in product_data.image_set.all()]
+                    }
+                )
             print(results)
             return JsonResponse({'result':results}, status=201)
 
