9c9b823492c05ca0adbf4b13cf955a692f6ccdd3
Ted0527 <frosthowling@gmail.com>
2021-12-08 17:50:54 +0900

    [fixing]

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,10 @@
 from django.urls    import path
 
-from products.views import ProductView, CategoryView
+from products.views import ProductView
 from .views import ProductDetailView
 
 urlpatterns = [
     path('', ProductView.as_view()),
-    path('/category', CategoryView.as_view()),
+    # path('/category', CategoryView.as_view()),
     path("/<int:product_id>",ProductDetailView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -9,101 +9,36 @@ class ProductView(View):
     def get(self, request):
 
         try:
-            menu_name = request.GET.get('menu', None)
-            sort      = request.GET.get('sort', 'created_at')
+            menu_name     = request.GET.get('menu', '채소')
+            category_name = request.GET.get('category', '브로컬리') 
+            sort          = request.GET.get('sort', '-created_at')
+            
             sort_dict = {
-                'created_at' : 'created_at',
-                'price'      : 'price',
-                '-price'     : '-price'
+                '-created_at' : '-created_at',
+                'price'       : 'price',
+                '-price'      : '-price'
             }
 
-            if menu_name   == '채소전체':
-                menu_name  = '채소'
-                menus      = Menu.objects.get(name=menu_name)
-                categories = Category.objects.filter(menu=menus).all()
-                products   = Product.objects.filter(category__in=categories).all().order_by(sort_dict[sort])
-                results    = []
-
-            elif menu_name == '샐러드전체':
-                menu_name  = '샐러드'
-                menus      = Menu.objects.get(name=menu_name)
-                categories = Category.objects.filter(menu=menus).all()
-                products   = Product.objects.filter(category__in=categories).all().order_by(sort_dict[sort])
-                results    = []
-
-            elif menu_name == '과일전체':
-                menu_name  = '과일'
-                menus      = Menu.objects.get(name=menu_name)
-                categories = Category.objects.filter(menu=menus).all()
-                products   = Product.objects.filter(category__in=categories).all().order_by(sort_dict[sort])
-                results    = []
-
-            elif menu_name == '간편식전체':
-                menu_name  = '간편식'
-                menus      = Menu.objects.get(name=menu_name)
-                categories = Category.objects.filter(menu=menus).all()
-                products   = Product.objects.filter(category__in=categories).all().order_by(sort_dict[sort])
-                results    = []
+            # offset
+            # limit
 
             if menu_name:
-                menus      = Menu.objects.get(name=menu_name)
-                categories = Category.objects.filter(menu=menus).all()
-                products   = Product.objects.filter(category__in=categories).all().order_by(sort_dict[sort])
-                # products = Product.objects.filter(category__menu_id=menu_name).all()
-                results    = []
+                menu     = Menu.objects.get(name=menu_name)
+                products = Product.objects.filter(category__menu=menu).order_by(sort_dict[sort])
 
-            for product_data in products:
-                results.append(
-                    {
-                        'name'         : product_data.name,
-                        'introduction' : product_data.introduction,
-                        'price'        : product_data.price,
-                        'image'        : [image.url for image in product_data.image_set.all()]
-                    }
-                )
-            
-            return JsonResponse({'result':results}, status=201)
-
-        except AttributeError:
-            return JsonResponse({'message' : 'AttributeError'}, status=400)
-
-        except KeyError:
-            return JsonResponse({'message' : 'KeyError'}, status=400)
-        
-        except TypeError:
-            return JsonResponse({'message' : 'TypeError'}, status=400)
-
-        except Category.DoesNotExist:
-            return JsonResponse({'message' : 'DoesNotExits'}, status=500)
-
-class CategoryView(View):
-    def get(self, request):
-        try:
-            category_name = request.GET.get('category', None) 
-            sort          = request.GET.get('sort', 'created_at')
-            sort_dict     = {
-                'created_at' : 'created_at',
-                'price'      : 'price',
-                '-price'     : '-price'
-            }
-                
             if category_name:
-                categories = Category.objects.get(name=category_name)
-                products   = Product.objects.filter(category=categories).all().order_by(sort_dict[sort])
-                # products = Product.objects.filter(category_id=category_id, category__menu_id=menu_id).all().order_by(sort_dict[sort])
-                results    = []
+                category = Category.objects.get(name=category_name)
+                products = category.product_set.all().order_by(sort_dict[sort])
 
-            for product_data in products:
-                results.append(
-                    {
-                        'name'         : product_data.name,
-                        'introduction' : product_data.introduction,
-                        'price'        : product_data.price,
-                        'image'        : [image.url for image in product_data.image_set.all()]
-                    }
-                )
-            
-            return JsonResponse({'result':results}, status=201)
+            results = [
+                {
+                    'name'         : product_data.name,
+                    'introduction' : product_data.introduction,
+                    'price'        : product_data.price,
+                    'image'        : [image.url for image in product_data.image_set.all()]
+                }for product_data in products]
+             
+            return JsonResponse({'result':results}, status=200)
 
         except AttributeError:
             return JsonResponse({'message' : 'AttributeError'}, status=400)
